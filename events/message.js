bc92e0d4f276a5a66af607b904391a4f3b6b3948
Midas <31105991+macxb@users.noreply.github.com>
2020-07-01 15:18:22 +0200

    Add files via upload

---
+++ events/message.js
@@ -0,0 +1,24 @@
+﻿const Discord = require("discord.js");
+const config = require("../config.json");
+
+
+module.exports = async (client, message) => {
+  const prefix  = config.prefix;
+  // Ignore all bots
+  if (message.author.bot) return;
+  
+  if (message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  client.channels.get("IDCANALREGISTRODELBOT").send(`Comando: /${command}\nServer: ${message.guild}\n Canal: ${message.channel}\nUsuario: ${message.author}\n${message.createdAt}.`)
+  
+  const cmd = client.commands.get(command);
+
+  if (!cmd) {
+    return message.reply("**Ese comando no existe... :expressionless: **\n:robot: :arrows_clockwise: :interrobang: ");
+  }
+
+  cmd.run(client, message, args);
+};
